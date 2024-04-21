9fda907626c206e8c3c41e66e4abe9ea0e2a57dc
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-04-21 16:24:58 +0900

    4月21日Vueの学習

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-let app5 = new Vue({
-  el: '#app-5',
+let app6 = new Vue({
+  el: '#app-6',
   data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
+    message: 'Hello Vue!'
   }
 })
-// methodsに定義するのはイベント
-// dataに定義するのはHTML内で使用する値
